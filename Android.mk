fa7707e062b01452c729595315e6ae3ad112f115
Sergi Sevil <sergi.sevil@gi-de.com>
2015-01-13 12:27:06 +0100

    Refactor aidl files

--- Android.mk
+++ Android.mk
@@ -2,13 +2,12 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += \
-      src/org/simalliance/openmobileapi/uiccterminal/ITerminalService.aidl
 
 LOCAL_PACKAGE_NAME := UiccTerminal
 LOCAL_CERTIFICATE := platform
 
-LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_JAVA_LIBRARIES := framework org.simalliance.openmobileapi
+
 LOCAL_DEX_PREOPT := false
 
 include $(BUILD_PACKAGE)
